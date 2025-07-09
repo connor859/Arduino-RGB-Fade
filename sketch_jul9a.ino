066aa3c9245606f4ac98ebd0363d2f838e7f38ad
Connor Brancato <connorbrancato@gmail.com>
2025-07-09 01:21:21 -0400

    CODE

--- sketch_jul9a.ino
+++ sketch_jul9a.ino
@@ -1,9 +1,32 @@
+int red = 9;
+ int green = 10;
+ int blue = 11;
 void setup() {
-  // put your setup code here, to run once:
+ pinMode(red, OUTPUT);
+ pinMode(green, OUTPUT);
+ pinMode(blue, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(int i = 0; i <= 255; i++)
+  {
+analogWrite(red, i * i / 255);
+analogWrite(blue, i / 2);
+analogWrite(green, 255 - i);
+ delay(10);
+  }
+  for(int i = 255; i >= 0; i--)
+  {
+analogWrite(red, i * i / 255);
+analogWrite(blue, i / 2);
+analogWrite(green, 255 - i);
+ delay(10);
+  }
 
+
+
+
+
+  
 }
